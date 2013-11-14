c9508987a9328b2b9dea58eb70da2b4de2edb25e
Miah Johnson <miah@chia-pet.org>
2013-11-13 20:31:47 -0800

    clean up that ugly

--- test/spec/spec_helpers.rb
+++ test/spec/spec_helpers.rb
@@ -9,7 +9,3 @@ require 'chef/resource/service'
 require 'chef/provider'
 require 'minitest/autorun'
 require 'minitest/pride'
-
-project_root = File.dirname( File.absolute_path( __FILE__ ))
-libraries_dir = File.join( '', project_root, '/../../libraries/' )
-Dir.glob( libraries_dir + '*.rb' ) { |file| require file }
