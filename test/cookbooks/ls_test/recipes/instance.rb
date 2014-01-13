a01da8507f8a839568252a6926bfadd271cfccfa
Miah Johnson <miah@chia-pet.org>
2014-01-12 16:16:19 -0800

    1.9 syntax.

--- test/cookbooks/ls_test/recipes/instance.rb
+++ test/cookbooks/ls_test/recipes/instance.rb
@@ -1,9 +1,9 @@
 logstash_instance 'test' do
   install_type 'jar'
-  install_options({
+  install_options(
     url: node.logstash.install_options.url,
     checksum: node.logstash.install_options.checksum,
     version: node.logstash.install_options.version
-  })
+  )
   service_type 'init'
 end
