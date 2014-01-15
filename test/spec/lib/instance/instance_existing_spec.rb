9040b9e10bb0dd4aad9ed942cd3f92cdc0d73ad5
Miah Johnson <miah@chia-pet.org>
2014-01-14 16:02:26 -0800

    start hacking our spec for instance_existing.

---
+++ test/spec/lib/instance/instance_existing_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../../spec_helpers'
+require 'chef_logstash/helpers'
+
+describe 'LogstashInstanceExisting', 'Tests for Logstash::Instance::Existing' do
+  describe 'Public instance methods.' do
+    describe '#install' do
+      it 'Puts Jar file in correct location.' do
+      end
+    end
+
+    describe '#uninstall' do
+      it 'Removes Jar file from correct location.' do
+      end
+    end
+  end
+end
